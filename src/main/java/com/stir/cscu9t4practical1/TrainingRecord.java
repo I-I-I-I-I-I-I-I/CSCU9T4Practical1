ff855d32677fa2c62d576d896d98d56b852f53a7
I-I-I-I-I-I-I-I <patrick_c321@hotmail.co.uk>
2021-02-04 10:52:44 +0000

    Part 2 finished. Moving to optional task 1, using a formatted text field import. :)

--- src/main/java/com/stir/cscu9t4practical1/TrainingRecord.java
+++ src/main/java/com/stir/cscu9t4practical1/TrainingRecord.java
@@ -7,7 +7,7 @@ package com.stir.cscu9t4practical1;
 import java.util.*;
 
 
-public class TrainingRecord {
+public class TrainingRecord  {
     private List<Entry> tr;
     
     public TrainingRecord() {
@@ -18,16 +18,68 @@ public class TrainingRecord {
    public void addEntry(Entry e){
        tr.add(e);    
    } // addClass
+
+   public String lookupAll (int d , int m , int y)
+    {
+
+        ListIterator<Entry> iter = tr.listIterator();
+        String all;
+        String noValue = "No entries found for this date.";
+        StringBuilder allBuild = new StringBuilder();
+        boolean check = true;
+
+        while(iter.hasNext())
+            {          
+                Entry currentValue = iter.next();
+                int monthCheck = currentValue.getMonth();
+                int dayCheck = currentValue.getDay();
+                int yearCheck = currentValue.getYear();
+                
+             if (currentValue.getDay()==d && currentValue.getMonth()==m && currentValue.getYear() == y)
+                
+                {
+                    allBuild.append(currentValue.getEntry() + " \n");   
+                }
+
+                check = false;
+
+            }
+
+        all = allBuild.toString();
+
+        if (check == false)
+            {
+                 return all;
+            }
+        else
+            {
+                return noValue;
+            }
+    }
    
    // look up the entry of a given day and month
-   public String lookupEntry (int d, int m, int y) {
+   public String[] lookupEntry (int d, int m, int y) {
+
        ListIterator<Entry> iter = tr.listIterator();
-       String result = "No entries found";
+       String[] result = {"No entries found" , ""};
+       int ifCounter = 0;
+
        while (iter.hasNext()) {
+
           Entry current = iter.next();
-          if (current.getDay()==d && current.getMonth()==m && current.getYear()==y) 
-             result = current.getEntry();
-            }
+
+                if (current.getDay()==d && current.getMonth()==m && current.getYear()==y) 
+                    {
+                        ifCounter++;
+                        if(ifCounter > 0)
+                            {
+                                result[1] = "trigger";
+                            }
+
+                        result[0] = current.getEntry();
+                    }
+                    
+        }
        return result;
    } // lookupEntry
    
--- src/main/java/com/stir/cscu9t4practical1/TrainingRecordGUI.java
+++ src/main/java/com/stir/cscu9t4practical1/TrainingRecordGUI.java
@@ -4,6 +4,9 @@ package com.stir.cscu9t4practical1;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
+import java.util.Spliterators.AbstractDoubleSpliterator;
+import javax.swing.JFormattedTextField;
+
 import javax.swing.*;
 import java.lang.Number;
 
@@ -11,6 +14,7 @@ public class TrainingRecordGUI extends JFrame implements ActionListener {
 
     private JTextField name = new JTextField(30);
     private JTextField day = new JTextField(2);
+    int t  = 2;
     private JTextField month = new JTextField(2);
     private JTextField year = new JTextField(4);
     private JTextField hours = new JTextField(2);
@@ -24,9 +28,16 @@ public class TrainingRecordGUI extends JFrame implements ActionListener {
     private JLabel labh = new JLabel(" Hours:");
     private JLabel labmm = new JLabel(" Mins:");
     private JLabel labs = new JLabel(" Secs:");
+    private JLabel labWarning = new JLabel("More than one record on this date. Showing most recent entry.");
     private JLabel labdist = new JLabel(" Distance (km):");
     private JButton addR = new JButton("Add");
     private JButton lookUpByDate = new JButton("Look Up");
+    private JButton findAllByDate = new JButton("Find all");
+    private JButton showAll = new JButton("Show me All entries on this date");
+    public int setLength = 500;
+    public int dStore;
+    public int mStore;
+    public int yStore;
 
     private TrainingRecord myAthletes = new TrainingRecord();
 
@@ -68,9 +79,16 @@ public class TrainingRecordGUI extends JFrame implements ActionListener {
         addR.addActionListener(this);
         add(lookUpByDate);
         lookUpByDate.addActionListener(this);
+        add(findAllByDate);
+        findAllByDate.addActionListener(this);
+        add(showAll);
+        showAll.addActionListener(this);
+        add(labWarning);
+        labWarning.setVisible(false);
         add(outputArea);
+        showAll.setVisible(false);
         outputArea.setEditable(false);
-        setSize(720, 200);
+        setSize(720, setLength);
         setVisible(true);
         blankDisplay();
 
@@ -88,8 +106,15 @@ public class TrainingRecordGUI extends JFrame implements ActionListener {
         if (event.getSource() == lookUpByDate) {
             message = lookupEntry();
         }
+        if (event.getSource() == findAllByDate) {
+            message = lookupAll();
+        }  
+        if (event.getSource() == showAll) {
+
+            message = showAll();
+        } 
         outputArea.setText(message);
-        blankDisplay();
+       blankDisplay();
     } // actionPerformed
 
     public String addEntry(String what) {
@@ -105,6 +130,7 @@ public class TrainingRecordGUI extends JFrame implements ActionListener {
         int s = Integer.parseInt(secs.getText());
         Entry e = new Entry(n, d, m, y, h, mm, s, km);
         myAthletes.addEntry(e);
+
         return message;
     }
     
@@ -113,9 +139,49 @@ public class TrainingRecordGUI extends JFrame implements ActionListener {
         int d = Integer.parseInt(day.getText());
         int y = Integer.parseInt(year.getText());
         outputArea.setText("looking up record ...");
-        String message = myAthletes.lookupEntry(d, m, y);
-        return message;
+
+        String returned[] = myAthletes.lookupEntry(d, m, y);
+
+        if (returned[1] == "trigger")
+            {
+                mStore = m;
+                dStore = d;
+                yStore = y;
+                Warning();
+            }
+
+        return myAthletes.lookupEntry(d, m, y)[0];
     }
+
+    /*Looks up and returns all entries for the entered date
+      Returns "No entry found" if date has no corresponding data
+      boolean atLeastOne checks if there is at least one entry.
+    */
+    public String lookupAll() {
+
+        int m = Integer.parseInt(month.getText());
+        int d = Integer.parseInt(day.getText());
+        int y = Integer.parseInt(year.getText());
+        outputArea.setText("looking up record ...");
+
+        String returner = myAthletes.lookupAll(d , m , y);
+        return returner;
+
+    }
+
+    /* If the "lookupEntry" button is clicked and the
+       inputted date has more than one entry, this method
+       will be invoked and warn the user of that fact.
+       Will display a button to give the user the option to
+       view all entries for this date (Just calls the lookupAll method above)
+    */
+
+    public void Warning()
+        {
+            labWarning.setVisible(true);
+            outputArea.setText("More than one record on this date. Showing most recent entry.");
+            showAll.setVisible(true);
+        }
 
     public void blankDisplay() {
         name.setText("");
@@ -139,6 +205,18 @@ public class TrainingRecordGUI extends JFrame implements ActionListener {
         secs.setText(String.valueOf(ent.getSec()));
         dist.setText(String.valueOf(ent.getDistance()));
     }
+
+    //Nothing to see here...
+    public String showAll()
+        {
+
+                int m = mStore;
+                int d = dStore;
+                int y = yStore;
+        
+                return myAthletes.lookupAll(d , m , y);
+        
+        }
 
 } // TrainingRecordGUI
 
